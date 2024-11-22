059fdf7530a65963e3b622dc111226d17af85421
Carlos Mesa <cmesaroldan@gmail.com>
2024-11-21 21:00:48 -0500

    Añadidas las contrasena de acceso

--- database/places.sql
+++ database/places.sql
@@ -30,3 +30,8 @@ CREATE TABLE TouristPlaces (
 INSERT INTO TouristPlaces (name, image_url, description, address) VALUES 
 ('Parque Explora', 'https://c8.alamy.com/comp/HMDA91/parque-explora-explore-park-interactive-science-museum-medellin-antioquia-HMDA91.jpg', 'Centro interactivo de ciencia y tecnología.', 'Cl. 52 #73-75, Medellín'),
 ('Pueblito Paisa', 'https://i.pinimg.com/originals/f4/d2/9e/f4d29e293a9d29874bf7e16128d62d3d.jpg', 'Representación de un típico pueblo antioqueño.', 'Cerro Nutibara, Medellín');
+
+
+INSERT INTO usuarios (nombre, email, password) VALUES 
+('Administrador', 'admin@lugares.com','$2a$10$zOqBTgbXFtbBX.KE1amYWu1iR3Guqg6sFOPESEkIpgt406imL67jO',TRUE,30),
+('Carlos', 'mesacarloste@gmail.com','$2a$10$IxcsAvm0vIUXw9/UU3s1DeIoGpQkO0LI/7vRiDtNlHPmnFOAS76Iq',FALSE,22);
